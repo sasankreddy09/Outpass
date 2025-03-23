fbb01ec5223b575c9bb4e2f3b0e5dccd197ea07a
sasank <sasankreddy09@gmail.com>
2025-03-23 14:27:12 +0530

    new commit

--- scripts/userseeding.js
+++ scripts/userseeding.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const User = require("../models/user");
+const User = require("../models/User");
 
 mongoose.connect("mongodb://localhost:27017/outpassDB", {
     useNewUrlParser: true,
